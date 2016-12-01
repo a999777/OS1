50d95242ec8cb2e187b08b4795fe381f0b0ddf40
Eitan-Lev <eitanlev@gmail.com>
2016-12-01 12:30:55 +0200

    Update signals

--- signals.cpp
+++ signals.cpp
@@ -7,3 +7,28 @@
    Synopsis: handle the Control-C */
 #include "signals.h"
 
+//SIGTSTP
+//**************************************************************************************
+// function name: Ctrl_z_handler
+// Description: handles the Ctrl-z signal - if process runs in fg - suspend it and add to joblist
+// Parameters: signum - signal number 
+// Returns: 
+//**************************************************************************************
+void handle_CTRL_z(int sig_num) {
+	
+	return;
+}
+
+//SIGINT
+//**************************************************************************************
+// function name: Ctrl_c_handler
+// Description: handles the Ctrl-c signal - terminates process if in fg , does nothing otherwise
+// Parameters: signum - signal number 
+// Returns: 
+//**************************************************************************************
+void handle_CTRL_c(int sig_num) {
+	return;
+}
+
+//SIGSTOP sent by kill(), can't be intercepted by process
+//SIGCONT un-suspends the process (counterpart to SIGTSTP & SIGSTOP)
